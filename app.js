6ca11c3575cac8d69d598ab9dbe991dba4a82a36
narasace <narasace@gmail.com>
2013-12-29 23:35:37 -0800

    updated files

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	static = require('node-static');
+var fileServer = new static.Server('./');
+app.listen(8000);
+function handler (request, response) {
+	request.addListener('end', function () {
+        fileServer.serve(request, response);
+    });
+}
+io.set('log level', 1);
+io.sockets.on('connection', function (socket) {
+	socket.on('mousemove', function (data) {
+		socket.broadcast.emit('moving', data);
+	});
+});
