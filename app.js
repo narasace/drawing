d044db6e4b6eb90a6a187913143ec929f4babc70
narasace <narasace@gmail.com>
2014-01-09 16:26:00 -0800

    modified app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+var port = process.env.PORT || 3000;
+
 var app = require('http').createServer(handler),
 	io = require('socket.io').listen(app),
 	static = require('node-static');
 var fileServer = new static.Server('./');
-app.listen($PORT);
+app.listen(port);
 function handler (request, response) {
 	request.addListener('end', function () {
         fileServer.serve(request, response);
